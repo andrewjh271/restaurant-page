16fe5e080b3741aa5c8d4210cd41855f9085c546
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2021-04-01 15:40:06 -0700

    Minor formatting improvements and refactoring

--- main.js
+++ main.js
@@ -19,18 +19,21 @@ __webpack_require__.r(__webpack_exports__);
   const heading1 = document.createElement('h3');
   const heading2 = document.createElement('h3');
   const heading3 = document.createElement('h3');
-  
+
   const p1 = document.createElement('p');
   const p2 = document.createElement('p');
   const p3 = document.createElement('p');
 
-  heading1.textContent = 'Our Story'
-  heading2.textContent = 'Our People'
-  heading3.textContent = 'Our Pasta'
+  heading1.textContent = 'Our Story';
+  heading2.textContent = 'Our People';
+  heading3.textContent = 'Our Pasta';
 
-  p1.textContent = 'We are Monzu Fresh Pasta, serving Italian and contemporary flavors made from scratch in the heart of San Diego’s East Village. In drawing upon our European heritage and passion for hand-crafted meals, we take pride in transporting you to Italy via our home country’s most beloved staple: fresh pasta. Monzu is a Neapolitan corruption of the French term “monsieur.” Neapolitan and Sicilian Monzu were renowned professional French-trained chefs who worked for aristocracy and members of the Bourbon court in 18th and 19th century Southern Italy. They became especially popular for their ability to mix French cuisine and Southern Italian ingredients. Their secret recipes, among them many different pasta specialties, are today part of the Italian food tradition.'
-  p2.textContent = 'Monzu Fresh Pasta is the family-owned creation of Neapolitan Aldo de Dominicis Rotondi and Serena Romano, a husband-wife team who share a strong belief in the Slow Food movement. Having trained with Italy\'s master pasta makers, Aldo and Serena lead the kitchen with centuries-old pasta making techniques and recipes hailing from distinct Italian regions including Sicily, Campania and Puglia.'
-  p3.textContent = 'Here you’ll discover authentic fresh pastas and sauces crafted in-house using local, organic and seasonal ingredients as well as pasture-raised eggs. From ravioli to tagliatelle and lasagna, every pasta is personalized with your choice of sauce, like Bolognese and Ligurian pesto. Plus, enjoy daily and weekly specials and savor the fact that several of our dishes cater to dairy-free, vegetarian and vegan diets.'
+  p1.textContent =
+    'We are Monzu Fresh Pasta, serving Italian and contemporary flavors made from scratch in the heart of San Diego’s East Village. In drawing upon our European heritage and passion for hand-crafted meals, we take pride in transporting you to Italy via our home country’s most beloved staple: fresh pasta. Monzu is a Neapolitan corruption of the French term “monsieur.” Neapolitan and Sicilian Monzu were renowned professional French-trained chefs who worked for aristocracy and members of the Bourbon court in 18th and 19th century Southern Italy. They became especially popular for their ability to mix French cuisine and Southern Italian ingredients. Their secret recipes, among them many different pasta specialties, are today part of the Italian food tradition.';
+  p2.textContent =
+    "Monzu Fresh Pasta is the family-owned creation of Neapolitan Aldo de Dominicis Rotondi and Serena Romano, a husband-wife team who share a strong belief in the Slow Food movement. Having trained with Italy's master pasta makers, Aldo and Serena lead the kitchen with centuries-old pasta making techniques and recipes hailing from distinct Italian regions including Sicily, Campania and Puglia.";
+  p3.textContent =
+    'Here you’ll discover authentic fresh pastas and sauces crafted in-house using local, organic and seasonal ingredients as well as pasture-raised eggs. From ravioli to tagliatelle and lasagna, every pasta is personalized with your choice of sauce, like Bolognese and Ligurian pesto. Plus, enjoy daily and weekly specials and savor the fact that several of our dishes cater to dairy-free, vegetarian and vegan diets.';
 
   container.appendChild(heading1);
   container.appendChild(p1);
@@ -41,6 +44,7 @@ __webpack_require__.r(__webpack_exports__);
 
   return container;
 }
+
 
 /***/ }),
 
@@ -70,7 +74,7 @@ __webpack_require__.r(__webpack_exports__);
   heading2.textContent = 'Phone';
   heading3.textContent = 'Email';
 
-  p1.textContent = '455 10th Avenue • San Diego • CA, 92101';
+  p1.innerHTML = '455 10th Avenue <br> San Diego, CA 92101';
   p2.textContent = '619.255.5032';
   p3.textContent = 'info@monzufreshpasta.com';
 
@@ -83,6 +87,7 @@ __webpack_require__.r(__webpack_exports__);
 
   return container;
 }
+
 
 /***/ }),
 
@@ -101,7 +106,7 @@ __webpack_require__.r(__webpack_exports__);
   container.classList.add('homepage-container');
 
   const heading = document.createElement('h1');
-  heading.textContent = 'Monzú Fresh Pasta'
+  heading.textContent = 'Monzú Fresh Pasta';
 
   const tagline = document.createElement('h4');
   tagline.textContent = 'Made from scratch in San Diego';
@@ -111,6 +116,7 @@ __webpack_require__.r(__webpack_exports__);
 
   return container;
 }
+
 
 /***/ }),
 
@@ -134,22 +140,25 @@ __webpack_require__.r(__webpack_exports__);
   const plate1 = document.createElement('h3');
   plate1.textContent = 'FIORILLI PLATE | 15';
   const description1 = document.createElement('p');
-  description1.textContent = 'Deep fried zucchini blossoms stuffed with cheese and mozzarella, served with a side of zucchini spaghetti, arugula and tomatoes'
+  description1.textContent =
+    'Deep fried zucchini blossoms stuffed with cheese and mozzarella, served with a side of zucchini spaghetti, arugula and tomatoes';
 
   const plate2 = document.createElement('h3');
   plate2.textContent = 'GRILLED SCALLOPS | 14';
   const description2 = document.createElement('p');
-  description2.textContent = 'Served over our pesto vinaigrette and toasted focaccia';
+  description2.textContent =
+    'Served over our pesto vinaigrette and toasted focaccia';
 
   const plate3 = document.createElement('h3');
   plate3.textContent = 'CALAMARI FRITTI | 12';
   const description3 = document.createElement('p');
-  description3.textContent = 'Fried calamari served with our marinara sauce'
+  description3.textContent = 'Fried calamari served with our marinara sauce';
 
   const plate4 = document.createElement('h3');
   plate4.textContent = 'CLASSIC BURRATA SALAD | 15';
   const description4 = document.createElement('p');
-  description4.textContent = 'Spring mix, burrata cheese, tomatoes, Italian olives, extra virgin olive oil, balsamic galze'
+  description4.textContent =
+    'Spring mix, burrata cheese, tomatoes, Italian olives, extra virgin olive oil, balsamic galze';
 
   const heading2 = document.createElement('h2');
   heading2.textContent = 'ITALIAN STYLE FLATBREADS';
@@ -157,17 +166,20 @@ __webpack_require__.r(__webpack_exports__);
   const plate5 = document.createElement('h3');
   plate5.textContent = 'ROSA | 15';
   const description5 = document.createElement('p');
-  description5.textContent = 'Red sauce, cherry tomatoes, mozzarella, basil, grated cheese'
+  description5.textContent =
+    'Red sauce, cherry tomatoes, mozzarella, basil, grated cheese';
 
   const plate6 = document.createElement('h3');
   plate6.textContent = 'SPICY CALABRIAN SAUSAGE | 16';
   const description6 = document.createElement('p');
-  description6.textContent = 'Red sauce, italian sausage, mushroom, Calabrian Nduja, mozzarella, grated cheese';
+  description6.textContent =
+    'Red sauce, italian sausage, mushroom, Calabrian Nduja, mozzarella, grated cheese';
 
   const plate7 = document.createElement('h3');
   plate7.textContent = 'TARTUFO | 17';
   const description7 = document.createElement('p');
-  description7.textContent = 'Burrata, truffle honey, truffle carpaccio, black pepper'
+  description7.textContent =
+    'Burrata, truffle honey, truffle carpaccio, black pepper';
 
   const column1 = document.createElement('div');
 
@@ -197,12 +209,14 @@ __webpack_require__.r(__webpack_exports__);
   const plate8 = document.createElement('h3');
   plate8.textContent = 'SPAGHETTI GAMBERI E LIMONE | 20';
   const description8 = document.createElement('p');
-  description8.textContent = 'Spaghetti served with a shrimp sauteed in a lemon butter and garlic sauce.';
+  description8.textContent =
+    'Spaghetti served with a shrimp sauteed in a lemon butter and garlic sauce.';
 
   const plate9 = document.createElement('h3');
   plate9.textContent = 'PISTACHIO PESTO GNOCCHI | 22';
   const description9 = document.createElement('p');
-  description9.textContent = 'Giant asiago-stuffed gnocchi served with a Sicilian Pistachio pesto, burrata flakes and cherry tomato';
+  description9.textContent =
+    'Giant asiago-stuffed gnocchi served with a Sicilian Pistachio pesto, burrata flakes and cherry tomato';
 
   const plate10 = document.createElement('h3');
   plate10.textContent = 'CASARECCE AL POMODORO | 15';
@@ -212,12 +226,14 @@ __webpack_require__.r(__webpack_exports__);
   const plate11 = document.createElement('h3');
   plate11.textContent = 'TAGLIATELLE ALLA BOLOGNESE | 17';
   const description11 = document.createElement('p');
-  description11.textContent = 'Tender slow-cooked ground beef and tomato sauce, served with cheese';
+  description11.textContent =
+    'Tender slow-cooked ground beef and tomato sauce, served with cheese';
 
   const plate12 = document.createElement('h3');
   plate12.textContent = 'PORCINI E SALSICCIA FUSILLI | 18';
   const description12 = document.createElement('p');
-  description12.textContent = 'Imported porcini mushrooms, fennel sausage, cream sauce, pepper and cheese';
+  description12.textContent =
+    'Imported porcini mushrooms, fennel sausage, cream sauce, pepper and cheese';
 
   const heading4 = document.createElement('h2');
   heading4.textContent = 'DESSERTS';
@@ -253,12 +269,13 @@ __webpack_require__.r(__webpack_exports__);
   column2.appendChild(plate14);
   column2.appendChild(plate15);
   column2.appendChild(plate16);
-  
+
   container.appendChild(column1);
   container.appendChild(column2);
 
   return container;
 }
+
 
 /***/ })
 
@@ -325,51 +342,52 @@ var __webpack_exports__ = {};
   !*** ./src/index.js ***!
   \**********************/
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _home_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home.js */ "./src/home.js");
-/* harmony import */ var _about_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./about.js */ "./src/about.js");
-/* harmony import */ var _menu_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./menu.js */ "./src/menu.js");
-/* harmony import */ var _contact_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./contact.js */ "./src/contact.js");
+/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home */ "./src/home.js");
+/* harmony import */ var _about__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./about */ "./src/about.js");
+/* harmony import */ var _menu__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./menu */ "./src/menu.js");
+/* harmony import */ var _contact__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./contact */ "./src/contact.js");
 
 
 
 
 
-const content = document.getElementById('content')
-content.appendChild((0,_home_js__WEBPACK_IMPORTED_MODULE_0__.default)());
+const content = document.getElementById('content');
+content.appendChild((0,_home__WEBPACK_IMPORTED_MODULE_0__.default)());
 
 const tabsHandler = (() => {
   const tabs = document.querySelectorAll('.tab');
 
   const clearActiveTabs = () => {
     tabs.forEach((tab) => tab.classList.remove('tab-active'));
-  }
+  };
 
   tabs.forEach((tab) => tab.addEventListener('click', clearActiveTabs));
 
   const homeTab = document.querySelector('.home');
   homeTab.addEventListener('click', () => {
     homeTab.classList.add('tab-active');
-    content.firstChild.replaceWith((0,_home_js__WEBPACK_IMPORTED_MODULE_0__.default)());
-  })
+    content.firstChild.replaceWith((0,_home__WEBPACK_IMPORTED_MODULE_0__.default)());
+  });
 
   const aboutTab = document.querySelector('.about');
   aboutTab.addEventListener('click', () => {
     aboutTab.classList.add('tab-active');
-    content.firstChild.replaceWith((0,_about_js__WEBPACK_IMPORTED_MODULE_1__.default)());
-  })
+    content.firstChild.replaceWith((0,_about__WEBPACK_IMPORTED_MODULE_1__.default)());
+  });
 
   const menuTab = document.querySelector('.menu');
   menuTab.addEventListener('click', () => {
     menuTab.classList.add('tab-active');
-    content.firstChild.replaceWith((0,_menu_js__WEBPACK_IMPORTED_MODULE_2__.default)());
-  })
+    content.firstChild.replaceWith((0,_menu__WEBPACK_IMPORTED_MODULE_2__.default)());
+  });
 
   const locationTab = document.querySelector('.contact');
   locationTab.addEventListener('click', () => {
     locationTab.classList.add('tab-active');
-    content.firstChild.replaceWith((0,_contact_js__WEBPACK_IMPORTED_MODULE_3__.default)());
-  })
+    content.firstChild.replaceWith((0,_contact__WEBPACK_IMPORTED_MODULE_3__.default)());
+  });
 })();
+
 })();
 
 /******/ })()
